728529c4ff5ddfcadbb49392aaeb439e6f036a8a
Bernat Ferragut <elbernat@gmail.com>
2017-01-09 07:02:36 -0400

    Lists Ex Finals

--- 1_Begginer/7_ListsExercices/7_ListsExercices/Program.cs
+++ 1_Begginer/7_ListsExercices/7_ListsExercices/Program.cs
@@ -8,7 +8,10 @@ namespace _ListsExercices
 		public static void Main(string[] args)
 		{
 			//Exercice1();
-			Exercice2();
+			//Exercice2b();
+			Exercice3();
+			//Exercice4();
+			//Exercice5();
 		}
 
 		//1- When you post a message on Facebook, depending on the number of people who like 
@@ -67,19 +70,34 @@ namespace _ListsExercices
 		//2- Write a program and ask the user to enter their name.Use an array to reverse 
 		//the name and then store the result in a new string. Display the reversed name on 
 		//the console.
-
+		//Mine
 		public static void Exercice2()
 		{
 			Console.Write("What's your name? ");
 			var name = Console.ReadLine();
 
+			var myArray = new char[name.Length];
+			myArray = name.ToCharArray();
+
+			Array.Reverse(myArray);
+
+			Console.WriteLine(myArray);
+		}
+		// From Mosh
+		public static void Exercice2b()
+		{
+			Console.Write("What's your name? ");
+			var name = Console.ReadLine();
+			//Array to reverse the name !!!
 			var array = new char[name.Length];
 			for (var i = name.Length; i > 0; i--)
-				array[name.Length - i] = name[i - 1];
+				array[name.Length - i] = name[i - 1];//Here converts the last letter to the first
 
 			var reversed = new string(array);
 			Console.WriteLine("Reversed name: " + reversed);
 		}
+
+
 
 		//3- Write a program and ask the user to enter 5 numbers.If a number has been previously 
 		//entered, display an error message and ask the user to re-try. Once the user successfully 
@@ -87,7 +105,25 @@ namespace _ListsExercices
 
 		public static void Exercice3()
 		{
+			var numbers = new List<int>();
 
+			while (numbers.Count < 5)
+			{
+				Console.Write("Enter a number: ");
+				var number = Convert.ToInt32(Console.ReadLine());
+				if (numbers.Contains(number))//Contains!!!
+				{
+					Console.WriteLine("You've previously entered " + number);
+					continue;
+				}
+
+				numbers.Add(number);
+			}
+
+			numbers.Sort();
+
+			foreach (var number in numbers)
+				Console.WriteLine(number);
 		}
 
 		//4- Write a program and ask the user to continuously enter a number or type "Quit" to 
@@ -96,7 +132,25 @@ namespace _ListsExercices
 
 		public static void Exercice4()
 		{
+			var myList = new List<int>();
 
+			while (true)
+			{
+				Console.WriteLine("Enter a number or type >Quit< to Exit: ");
+				var input = Console.ReadLine();
+
+				if (input == "Quit")
+				{
+					break;
+				}
+				else
+				{
+					var number = Convert.ToInt32(input);
+					myList.Add(number);
+				}
+			}
+			foreach(var n in myList)
+				Console.WriteLine(n);
 		}
 
 		//5- Write a program and ask the user to supply a list of comma separated numbers 
@@ -106,7 +160,28 @@ namespace _ListsExercices
 
 		public static void Exercice5()
 		{
+			Console.WriteLine("Enter coma separated numbers: ");
+			var numbers = Console.ReadLine();
 
+			var myList = new List<int>();
+
+			var splittedList  = numbers.Split(',');
+
+			foreach (var n in splittedList)
+			{
+				myList.Add(Convert.ToInt32(n));
+			}
+
+			if (myList.Count < 5)
+			{
+				Console.WriteLine("Invalid List, Please retry");
+			}
+			else
+			{
+				myList.Sort();
+				for (var i = 0; i <= 2; i++)
+					Console.WriteLine(myList[i]);
+			}
 		}
 	}
 }

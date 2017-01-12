d89056c9ad51a9919151ecf249f7d5b692637b7a
Bernat Ferragut <elbernat@gmail.com>
2017-01-12 08:35:54 -0400

    String 3

--- 1_Begginer/8_Strings2/8_Strings2/Program.cs
+++ 1_Begginer/8_Strings2/8_Strings2/Program.cs
@@ -8,32 +8,10 @@ namespace _Strings2
 		public static void Main(string[] args)
 		{
 			var sentence = "This is going to be a really really really really really really long text";
-			var summary = SummarizeText(sentence, 100);
+			var summary = StringUtility.SummarizeText(sentence, 100);
 			Console.WriteLine(summary);
 
 		}
 
-		//We create a function that holds all the logic
-		static string SummarizeText (string text, int maxLength = 20) 
-		{
-			if (text.Length < maxLength)
-				return text;
-
-			//sentence.Substring(0, maxNumber);
-			var words = text.Split(' ');
-			var totalChar = 0;
-			var summaryWords = new List<string>();
-
-			foreach (var word in words)
-			{
-				summaryWords.Add(word);
-
-				totalChar += words.Length + 1;
-				if (totalChar > maxLength)
-					break;
-			}
-
-			return String.Join(" ", summaryWords) + "...";
-		}
 	}
 }
---
+++ 1_Begginer/8_Strings2/8_Strings2/StringUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Strings2
+{
+	public class StringUtility
+	{
+
+		//We create a function that holds all the logic
+		public static string SummarizeText(string text, int maxLength = 20)
+		{
+			if (text.Length < maxLength)
+				return text;
+
+			//sentence.Substring(0, maxNumber);
+			var words = text.Split(' ');
+			var totalChar = 0;
+			var summaryWords = new List<string>();
+
+			foreach (var word in words)
+			{
+				summaryWords.Add(word);
+
+				totalChar += words.Length + 1;
+				if (totalChar > maxLength)
+					break;
+			}
+
+			return String.Join(" ", summaryWords) + "...";
+		}
+
+	}
+}

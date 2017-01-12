f60b51ac86899fafb2b70086ab83898fe26ddc86
Bernat Ferragut <elbernat@gmail.com>
2017-01-12 07:46:15 -0400

    Strings 2

---
+++ 1_Begginer/8_Strings2/8_Strings2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _Strings2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var sentence = "This is going to be a really really really really really really long text";
+			const int maxNumber = 20;
+
+			if (sentence.Length < maxNumber)
+				Console.WriteLine(sentence);
+			else
+			{
+				//sentence.Substring(0, maxNumber);
+			}
+
+		}
+	}
+}

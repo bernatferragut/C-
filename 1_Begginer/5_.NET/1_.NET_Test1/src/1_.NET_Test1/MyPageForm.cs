0d5c3a00e7038a12396a2b79d0c1ef3368ccac6e
Bernat Ferragut <elbernat@gmail.com>
2017-01-16 07:38:50 -0400

    Ex1

---
+++ 1_Begginer/5_.NET/1_.NET_Test1/src/1_.NET_Test1/MyPageForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace 1_.NET_Test1
+{
+	public class MyPageForm : ContentPage
+{
+	public MyPageForm()
+	{
+		Content = new StackLayout
+		{
+			Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+		};
+	}
+}
+}
+
--- 1_Begginer/8_StringsExercices/8_StringsExercices/Program.cs
+++ 1_Begginer/8_StringsExercices/8_StringsExercices/Program.cs
@@ -7,11 +7,11 @@ namespace _StringsExercices
 	{
 		public static void Main(string[] args)
 		{
-			Exercise1();
+			//Exercise1();
 			//Exercise2();
 			//Exercise3();
 			//Exercise4();
-			//Exercise5();
+			Exercise5();
 		}
 
 		//1- Write a program and ask the user to enter a few numbers separated by a hyphen.
@@ -31,7 +31,7 @@ namespace _StringsExercices
 			var isConsecutive = true;
 			for (var i = 1; i < numbers.Count; i++)
 			{
-				if (numbers[i] != numbers[i - 1] + 1)
+				if (numbers[i] != numbers[i - 1] + 1)//if you add one to a two consecutive numbers thry should be equal
 				{
 					isConsecutive = false;
 					break;
@@ -42,43 +42,5 @@ namespace _StringsExercices
 			Console.WriteLine(message);
 		}
 
-		//2- Write a program and ask the user to enter a few numbers separated by a hyphen.
-		//If the user simply presses Enter, without supplying an input, exit immediately; 
-		//otherwise, check to see if there are duplicates.If so, display "Duplicate" on the console.
-		public static void Exercice2()
-		{
-
-		}
-
-
-		//3- Write a program and ask the user to enter a time value in the 24-hour time format(e.g. 19:00). 
-		//A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; 
-		//otherwise, display "Invalid Time". If the user doesn't provide any values, 
-		//consider it as invalid time. 
-		public static void Exercice3()
-		{
-
-		}
-
-
-		//4- Write a program and ask the user to enter a few words separated by a space.
-		//Use the words to create a variable name with PascalCase.For example, if the user types: 
-		//"number of students", display "NumberOfStudents". 
-		//Make sure that the program is not dependent on the input.
-		//So, if the user types "NUMBER OF STUDENTS", the program should still display 
-		//"NumberOfStudents".
-		public static void Exercice4()
-		{
-
-		}
-
-
-		//5- Write a program and ask the user to enter an English word. 
-		//Count the number of vowels (a, e, o, u, i) in the word.
-		//So, if the user enters "inadequate", the program should display 6 on the console.
-		public static void Exercice5()
-		{
-
-		}
 	}
 }

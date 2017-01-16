9f1c719dce5bb017f914f256e01a07a02f9e0c59
Bernat Ferragut <elbernat@gmail.com>
2017-01-16 07:40:06 -0400

    Ex5

--- 1_Begginer/8_StringsExercices/8_StringsExercices/Program.cs
+++ 1_Begginer/8_StringsExercices/8_StringsExercices/Program.cs
@@ -117,6 +117,52 @@ namespace _StringsExercices
 			}
 		}
 
+		//4- Write a program and ask the user to enter a few words separated by a space.
+		//Use the words to create a variable name with PascalCase.For example, if the user types: 
+		//"number of students", display "NumberOfStudents". 
+		//Make sure that the program is not dependent on the input.
+		//So, if the user types "NUMBER OF STUDENTS", the program should still display 
+		//"NumberOfStudents".
+		public static void Exercise4()
+		{
+			Console.Write("Enter a few words: ");
+			var input = Console.ReadLine();
+
+			if (String.IsNullOrWhiteSpace(input))
+			{
+				Console.WriteLine("Error");
+				return;
+			}
+
+			var variableName = "";
+			foreach (var word in input.Split(' '))
+			{
+				var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1); // all magic
+				variableName += wordWithPascalCase;
+			}
+
+			Console.WriteLine(variableName);
+		}
+
+		//5- Write a program and ask the user to enter an English word. 
+		//Count the number of vowels (a, e, o, u, i) in the word.
+		//So, if the user enters "inadequate", the program should display 6 on the console.
+		public static void Exercise5()
+		{
+			Console.Write("Enter a word: ");
+			// Note the ToLower() here. This is used to count for both A and a. 
+			var input = Console.ReadLine().ToLower();
+
+			var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
+			var vowelsCount = 0;
+			foreach (var character in input)
+			{
+				if (vowels.Contains(character))
+					vowelsCount++;
+			}
+
+			Console.WriteLine(vowelsCount);
+		}
 
 	}
 }

481be3d0f9938a3fcc87a69528b37779a1a51efc
Bernat Ferragut <elbernat@gmail.com>
2017-01-16 07:39:47 -0400

    Ex4

--- 1_Begginer/8_StringsExercices/8_StringsExercices/Program.cs
+++ 1_Begginer/8_StringsExercices/8_StringsExercices/Program.cs
@@ -77,5 +77,46 @@ namespace _StringsExercices
 		}
 
 
+		//3- Write a program and ask the user to enter a time value in the 24-hour time format(e.g. 19:00). 
+		//A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; 
+		//otherwise, display "Invalid Time". If the user doesn't provide any values, 
+		//consider it as invalid time. 
+		public static void Exercise3()
+		{
+			Console.Write("Enter time: ");
+			var input = Console.ReadLine();
+			// We check the null input
+			if (String.IsNullOrWhiteSpace(input))
+			{
+				Console.WriteLine("Invalid Time");
+				return; // is break for if's
+			}
+
+			// We check the 2 components of the input
+			var components = input.Split(':');
+			if (components.Length != 2)
+			{
+				Console.WriteLine("Invalid Time");
+				return;
+			}
+
+			// Try catch exception for both numbers of the list
+			try
+			{
+				var hour = Convert.ToInt32(components[0]);
+				var minute = Convert.ToInt32(components[1]);
+
+				if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+					Console.WriteLine("Ok");
+				else
+					Console.WriteLine("Invalid Time");
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Invalid Time");
+			}
+		}
+
+
 	}
 }

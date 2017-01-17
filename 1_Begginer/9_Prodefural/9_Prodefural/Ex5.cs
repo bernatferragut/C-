a4c8f96fd1524299c03c48df9dacbd66e56c66c9
Bernat Ferragut <elbernat@gmail.com>
2017-01-17 07:54:35 -0400

    Procedurals 5 Option 1

--- 1_Begginer/9_Prodefural/9_Prodefural/Ex5.cs
+++ 1_Begginer/9_Prodefural/9_Prodefural/Ex5.cs
@@ -7,7 +7,29 @@ namespace _Procedurals
 	{
 		public static void Main(string[] args)
 		{ 
-			
+			Console.Write("Enter a word: ");
+			// Note the ToLower() here. This is used to count for both A and a. 
+			var input = Console.ReadLine().ToLower();
+
+			var result = IncludesVowels(input);
+
+			if ( result == true)
+				Console.WriteLine("Has vowels");
+
+			Console.WriteLine("Does not have vowels");
+		}
+
+		public static bool IncludesVowels(string input)
+		{
+			var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
+			var vowelsCount = 0;
+			foreach (var character in input)
+			{
+				if (vowels.Contains(character))
+					vowelsCount++;
+					return true;
+			}
+			return false;
 		}
 	}
 }

a76771663d45ca529faeb2c57d88d822358e0869
Bernat Ferragut <elbernat@gmail.com>
2017-01-17 07:45:46 -0400

    Procedurals 4 option 1

---
+++ 1_Begginer/9_Prodefural/9_Prodefural/Ex4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Procedurals
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter a few words: ");
+			var input = Console.ReadLine();
+
+
+			var result = WordWithPascaleCase(input);
+			Console.WriteLine(result);
+		}
+
+		public static string WordWithPascaleCase(string input)
+		{
+			if (String.IsNullOrWhiteSpace(input))
+			{
+				return "Error";
+			}
+
+			var variableName = "";
+			foreach (var word in input.Split(' '))
+			{
+				var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+				variableName += wordWithPascalCase;
+			}
+			return variableName;
+			
+		}
+	}
+}
--- 1_Begginer/9_Prodefural/9_Prodefural/Program.cs
+++ 1_Begginer/9_Prodefural/9_Prodefural/Program.cs
@@ -7,46 +7,7 @@ namespace _Procedurals
 	{
 		public static void Main(string[] args)
 		{
-			Console.Write("Enter time: ");
-			var input = Console.ReadLine();
-
-			if (String.IsNullOrWhiteSpace(input))
-			{
-				Console.WriteLine("Invalid Time");
-				return;
-			}
-
-			var components = input.Split(':');
-			if (components.Length != 2)
-			{
-				Console.WriteLine("Invalid Time");
-				return;
-			}
-			//
-			var result = TimeComparison(components);
-			if (result == true)
-				Console.WriteLine("Valid Time");
 			
-			Console.WriteLine("Invalid Time");
-
-		}
-
-		public static bool TimeComparison (string[] components)
-		{
-			try
-			{
-				var hour = Convert.ToInt32(components[0]);
-				var minute = Convert.ToInt32(components[1]);
-
-				if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
-					return true;
-				else
-					return false ;
-			}
-			catch (Exception)
-			{
-				return false;
-			}
 		}
 	}
 }

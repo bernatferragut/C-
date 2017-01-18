1aa04a0e645b415271ff4aac3885d18aaa49b8be
Bernat Ferragut <elbernat@gmail.com>
2017-01-18 07:22:12 -0400

    files 1

---
+++ 1_Begginer/10_Files1/10_Files1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace _Files1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Copy
+			//File.Copy("c:\\temp\\myfile.jpeg", "d:\\temp\\myfile.jpeg", true);
+			File.Copy(@"c:\temp\myfile.jpeg", @"d:\temp\myfile.jpeg", true);
+			// verbatim string
+		}
+	}
+}

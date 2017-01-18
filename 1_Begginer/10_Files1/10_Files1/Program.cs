dc66a84c816dd4bf881bbfd82949de4431f401da
Bernat Ferragut <elbernat@gmail.com>
2017-01-18 07:24:11 -0400

    files 2

--- 1_Begginer/10_Files1/10_Files1/Program.cs
+++ 1_Begginer/10_Files1/10_Files1/Program.cs
@@ -7,10 +7,16 @@ namespace _Files1
 	{
 		public static void Main(string[] args)
 		{
+
+			var path = @"c:\temp\myfile.jpeg;
+
 			//Copy
 			//File.Copy("c:\\temp\\myfile.jpeg", "d:\\temp\\myfile.jpeg", true);
 			File.Copy(@"c:\temp\myfile.jpeg", @"d:\temp\myfile.jpeg", true);
 			// verbatim string
+
+			//Delete
+			File.Delete(path);
 		}
 	}
 }

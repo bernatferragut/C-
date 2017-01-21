ffecd32f21aaa5a183fe7b9e4cb3683fb8d38986
Bernat Ferragut <elbernat@gmail.com>
2017-01-21 08:26:55 -0400

    OtherSolution v1

---
+++ 1_Begginer/12_FilesExercices/OtherSolutions/OtherSolutions.cs
@@ -0,0 +1,30 @@
+// Exercice 1
+
+using System;
+using System.IO;
+using System.Linq;
+namespace  Exercise_1
+{    
+class Program 
+   {     
+   static void Main() 
+       { 
+            var path = @"C:\Users\Sarxan\Documents\test.txt"; 
+            Console.WriteLine("number of words:"+NumberofWords(path)); 
+        }
+    public static int NumberofWords(string textfilepath)
+    {  
+        char[] chars = {' ', ',', '.', '?', '!',';', ':', '\'','-','/','=' };
+        var text = File.ReadAllText(textfilepath).Replace("\r\n"," "); 
+        var count = 0;  
+        for (var i = 0; i < text.Length-1; i++) 
+        {
+            if (!chars.Contains(text[i])&&chars.Contains(text[i+1]))  // check for double,tripple whitespaces,comma etc  
+        {   
+        count++;   
+    }  
+}  
+         return count; 
+}  
+  }
+}

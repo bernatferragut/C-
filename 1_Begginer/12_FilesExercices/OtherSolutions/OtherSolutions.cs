98e9cb23420f48dea77f5f37bd7e6d647308b7dd
Bernat Ferragut <elbernat@gmail.com>
2017-01-21 08:30:33 -0400

    OtherSolution version 2

--- 1_Begginer/12_FilesExercices/OtherSolutions/OtherSolutions.cs
+++ 1_Begginer/12_FilesExercices/OtherSolutions/OtherSolutions.cs
@@ -5,26 +5,36 @@ using System.IO;
 using System.Linq;
 namespace  Exercise_1
 {    
-class Program 
-   {     
-   static void Main() 
-       { 
+    class Program
+    {     
+        static void Main()
+        { 
             var path = @"C:\Users\Sarxan\Documents\test.txt"; 
             Console.WriteLine("number of words:"+NumberofWords(path)); 
         }
-    public static int NumberofWords(string textfilepath)
-    {  
-        char[] chars = {' ', ',', '.', '?', '!',';', ':', '\'','-','/','=' };
-        var text = File.ReadAllText(textfilepath).Replace("\r\n"," "); 
-        var count = 0;  
-        for (var i = 0; i < text.Length-1; i++) 
-        {
-            if (!chars.Contains(text[i])&&chars.Contains(text[i+1]))  // check for double,tripple whitespaces,comma etc  
-        {   
-        count++;   
-    }  
-}  
-         return count; 
-}  
-  }
+        public static int NumberofWords(string textfilepath)
+        {  
+            char[] chars = {' ', ',', '.', '?', '!',';', ':', '\'','-','/','=' };
+            var text = File.ReadAllText(textfilepath).Replace("\r\n"," "); 
+            var count = 0;  
+            for (var i = 0; i < text.Length-1; i++) 
+            {
+                if (!chars.Contains(text[i])&&chars.Contains(text[i+1]))  // check for double,tripple whitespaces,comma etc  
+            {   
+            count++;   
+        }  
+        return count;
+    }
+}
+// Version 2 Hacked
+
+staticvoidMain(string[] args)
+{
+var path =@"C:\Users\Sarxan\Documents\test.txt";
+Console.WriteLine("number of words:"+SplitWords(path).Length);
+}
+publicstaticstring[]SplitWords(string filename)
+{    
+        filename=File.ReadAllText(filename).Replace("\r\n"," ");
+returnRegex.Split(filename,@"\W+");
 }

21599e260f9826ce49c34ff5f9b6ff00a046caad
Bernat Ferragut <elbernat@gmail.com>
2017-01-25 08:57:47 -0400

    Constructors

---
+++ 2_Intermediate/1_Classes A/ClassesA/ClassesA/Customer.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClassesA
+{
+	// Constructors
+	public class Customer
+	{
+		public int Id;
+		public string Name;
+	}
+}
--- 2_Intermediate/1_Classes A/ClassesA/ClassesA/Program.cs
+++ 2_Intermediate/1_Classes A/ClassesA/ClassesA/Program.cs
@@ -7,8 +7,15 @@ namespace ClassesA
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-		}
 
-		// Constructors
+			var Customer = new Customer();
+			//Customer.Id = 1;
+			//Customer.Name = "Ber";
+
+			Console.WriteLine(Customer.Id);
+			Console.WriteLine(Customer.Name);
+		}
 	}
+
+
 }

68a973843b4cc1a4199014bef3d4aff52608315c
Bernat Ferragut <elbernat@gmail.com>
2017-01-26 08:46:19 -0400

    actors

--- 2_Intermediate/1_Classes A/ObjectInitializers/ObjectInitializers/Program.cs
+++ 2_Intermediate/1_Classes A/ObjectInitializers/ObjectInitializers/Program.cs
@@ -12,4 +12,20 @@ namespace ObjectInitializers
 			Console.WriteLine("Hello World!");
 		}
 	}
+
+
+	// ctor: To initialize an object and put it into an early state.
+	// object initializer: A syntax to initialize an object without the need to 
+	// initialize one of its constructors.
+	// Why? To avoid creating multiple ctors.
+
+	public class Person1
+	{
+		public int Id;
+		public string FirstName;
+		public string SecondName;
+		public DateTime BirthDate;
+
+		// ctor1
+	}
 }

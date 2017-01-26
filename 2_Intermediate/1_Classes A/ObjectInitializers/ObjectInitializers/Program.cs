9347ec8c75cfd6fbfda79eadcd7dbb3cbf62b83d
Bernat Ferragut <elbernat@gmail.com>
2017-01-26 08:40:26 -0400

    Object Initializers

---
+++ 2_Intermediate/1_Classes A/ObjectInitializers/ObjectInitializers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObjectInitializers
+{
+
+	// Object Initializers
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}

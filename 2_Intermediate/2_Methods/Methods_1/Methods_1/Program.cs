632a283c33c3de48da452c80875fa6c7c84ff46f
Bernat Ferragut <elbernat@gmail.com>
2017-01-27 06:55:09 -0400

    Methods

--- 2_Intermediate/2_Methods/Methods_1/Methods_1/Program.cs
+++ 2_Intermediate/2_Methods/Methods_1/Methods_1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Methods_1
 {
@@ -6,7 +8,22 @@ namespace Methods_1
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Console.WriteLine("Hello World!");
 		}
+	}
+
+	public class Point
+	{
+		int x;
+
+		int y;
+
+		public void Move(int x, int y) { }
+
+		//Overloading
+
+		public void Move(Point newLocation) { }
+
+		public void Move(Point newLocation, int speed) { }
 	}
 }

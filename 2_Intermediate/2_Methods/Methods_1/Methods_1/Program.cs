bdbed483c4e0dcc9f1eb1f86edbfdd211d172921
Bernat Ferragut <elbernat@gmail.com>
2017-01-27 07:08:23 -0400

    Methods

--- 2_Intermediate/2_Methods/Methods_1/Methods_1/Program.cs
+++ 2_Intermediate/2_Methods/Methods_1/Methods_1/Program.cs
@@ -10,6 +10,9 @@ namespace Methods_1
 		{
 			var myPoint = new Point();
 			myPoint.Move(3, 4);
+
+			var myCalculator = new Calculator();
+			myCalculator.Add(new int[] { 0, 1020, 30, 44 });
 		}
 	}
 
@@ -26,5 +29,17 @@ namespace Methods_1
 		public void Move(Point newLocation) { }
 
 		public void Move(Point newLocation, int speed) { }
+	}
+
+	public class Calculator
+	{
+		//public int Add(int n1, int n2) { }
+		//public int Add(int n1, int n2, int n3) { }
+		//public int Add(int n1, int n2, int n3, int n4) { }
+
+		// using an Array to Oveload
+
+		public int Add(int[] numbers) { }
+
 	}
 }

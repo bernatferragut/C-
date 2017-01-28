5fab14471b2b82ac535ae8a3b0f54663f7824e43
Bernat Ferragut <elbernat@gmail.com>
2017-01-28 09:48:15 -0400

    Fields

---
+++ 2_Intermediate/3_Fields/Fields/Fields/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fields
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	public class Customer
+	{
+		// Initialize Order method 2
+		List<Order> Orders = new List<Order>();
+
+		public Customer()
+		{
+			// Initialize Order method 1
+			//Orders = new List<Order>();
+	
+		}
+
+}

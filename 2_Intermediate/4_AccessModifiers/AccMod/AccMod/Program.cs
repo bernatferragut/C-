1065aeee12eba792b191c879b70325eedd9a4e76
Bernat Ferragut <elbernat@gmail.com>
2017-01-30 08:10:00 -0400

    Access Modifiers aka AM

---
+++ 2_Intermediate/4_AccessModifiers/AccMod/AccMod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AccMod
+{
+	// 1.Public
+	// 2.Private
+	// 3.Protected
+	// 4.Internal
+	// 5.Protected Internal
+	// A way to control access to a class and or its memebers ti improve robustness
+
+	// A. ENCAPSULATION or INFORMATION HIDING
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+		}
+	}
+
+	public class Person
+	{
+		private string Name;
+
+		public void GetName(string name)
+		{
+			if (!String.IsNullOrWhiteSpace(name)) // we include a condition
+			{
+				this.Name = name;
+			}
+		}
+
+		public string SetName()
+		{
+			return Name;
+		}
+	}
+}

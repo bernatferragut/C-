b0ce9a2ecf66a7f05cf31902d2abac37af2d04fe
Bernat Ferragut <elbernat@gmail.com>
2017-01-31 08:26:19 -0400

    Properties

--- 2_Intermediate/5_Properties/Props/Props/Program.cs
+++ 2_Intermediate/5_Properties/Props/Props/Program.cs
@@ -9,4 +9,19 @@ namespace Props
 			Console.WriteLine("Hello World!");
 		}
 	}
+
+	public class Person
+	{
+		private DateTime _birthdate;
+
+		public void SetBirth(DateTime Birthdate) // Setter
+		{
+			this._birthdate = Birthdate;
+		}
+
+		public DateTime SetBirth() // Getter
+		{
+			return _birthdate;
+		}
+	}
 }

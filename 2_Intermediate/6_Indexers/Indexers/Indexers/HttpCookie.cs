75a9c7d2487687eda236c03f3ef2b85699506ab2
Bernat Ferragut <elbernat@gmail.com>
2017-02-01 20:28:26 -0400

    indexer

--- 2_Intermediate/6_Indexers/Indexers/Indexers/HttpCookie.cs
+++ 2_Intermediate/6_Indexers/Indexers/Indexers/HttpCookie.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 namespace Indexers
 {
 
 	public class HttpCookie
 	{
-		
+		private Dictionary<string, string> _dicionary;
+
+		public HttpCookie()
+		{
+			_dicionary = new Dictionary<string, string>();
+		}
 	}
 }

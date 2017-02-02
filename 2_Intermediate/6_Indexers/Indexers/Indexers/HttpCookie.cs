986e037b7380f2f0bb0bb8721c0b449649ddba4a
Bernat Ferragut <elbernat@gmail.com>
2017-02-01 20:35:35 -0400

    indexer

--- 2_Intermediate/6_Indexers/Indexers/Indexers/HttpCookie.cs
+++ 2_Intermediate/6_Indexers/Indexers/Indexers/HttpCookie.cs
@@ -6,11 +6,17 @@ namespace Indexers
 
 	public class HttpCookie
 	{
-		private Dictionary<string, string> _dicionary;
+		private readonly Dictionary<string, string> _dicionary;
 
 		public HttpCookie()
 		{
 			_dicionary = new Dictionary<string, string>();
+		}
+
+		public string this[string key]
+		{
+			get { return _dicionary[key]; }
+			set { _dicionary[key] = value; }
 		}
 	}
 }

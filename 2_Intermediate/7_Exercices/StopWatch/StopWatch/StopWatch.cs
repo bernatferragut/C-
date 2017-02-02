57a77cbad2f3c70455cdc07d76a523feee3db047
Bernat Ferragut <elbernat@gmail.com>
2017-02-02 06:40:15 -0400

    SW

--- 2_Intermediate/7_Exercices/StopWatch/StopWatch/StopWatch.cs
+++ 2_Intermediate/7_Exercices/StopWatch/StopWatch/StopWatch.cs
@@ -1,30 +1,31 @@
 using System;
+using System.Timers;
 
 namespace StopWatch
 {
 
 	public class StopWatch
 	{
-		public DateTime _time{ get; private set;} // PROP
+		public Timer _time{ get; private set;} // PROP
 
-		public StopWatch(DateTime Time) // CTOR
+		public StopWatch(Timer Time) // CTOR
 		{
 			this._time = Time;
 		}
 
-		public int Play() // MTHD1
+		public Timer Play() // MTHD1
 		{
-			return _time.Hour;
+			return _time.Start();
 		}
 
-		public int Stop() // MTHD2
+		public Timer Stop() // MTHD2
 		{
-			return _time.Minute;
+			return _time.Stop();
 		}
 
-		public int ReStart() // MTHD3
+		public Timer ReStart() // MTHD3
 		{
-			return _time.Second;
+			return _time.AutoReset();
 		}
 	}
 }

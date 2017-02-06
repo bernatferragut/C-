0da064e0a0602fb9104a621b820fbec3b5b5330e
Bernat Ferragut <elbernat@gmail.com>
2017-02-06 07:22:29 -0300

    Inheritance

--- 2_Intermediate/8_Inheritance/8_Inheritance/Program.cs
+++ 2_Intermediate/8_Inheritance/8_Inheritance/Program.cs
@@ -15,5 +15,22 @@ namespace _Inheritance
 	// Polymorphism
 	// Parent or Super Class - vs - Child or Sub Class
 
+	public class PresentationObject
+	{
+		// Props
+		public int Width{ get; set; }
 
+		public int Hieght{ get; set; }
+
+		//Methods
+		public void Copy()
+		{
+			Console.WriteLine("Object Copied to clipboard");
+		}
+
+		public void Paste()
+		{
+			Console.WriteLine("Object was Pasted to file");
+		}
+	}
 }

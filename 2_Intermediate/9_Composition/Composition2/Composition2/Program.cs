98dff2b56bb67dbf8b8a0765599875f79f243ae2
Bernat Ferragut <elbernat@gmail.com>
2017-02-08 09:10:09 -0300

    Composition 2

--- 2_Intermediate/9_Composition/Composition2/Composition2/Program.cs
+++ 2_Intermediate/9_Composition/Composition2/Composition2/Program.cs
@@ -3,11 +3,16 @@
 
 // Favour Composition over Inheritance
 
-// Problems with Inheritance
+// Problems with Inheritance ( IS A Relationship )
 // 1- Easely abused by amateur developers
 // 2- Large Hierarchies
 // 3- Fragility
 // 4- Tightly Coupled
+
+// Compoistion ( HAS A Relationship )
+
+//1- We get great flexibility
+//2- Eventually loose coupling 
 
 
 namespace Composition2

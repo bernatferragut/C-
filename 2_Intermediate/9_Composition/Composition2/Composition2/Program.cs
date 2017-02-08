fc7b41185380af4d9a877be3356f00294317ca92
Bernat Ferragut <elbernat@gmail.com>
2017-02-08 09:05:05 -0300

    Composition 2

---
+++ 2_Intermediate/9_Composition/Composition2/Composition2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+// Favour Composition over Inheritance
+
+// Problems with Inheritance
+// 1- Easely abused by amateur developers
+// 2- Large Hierarchies
+// 3- Fragility
+// 4- Tightly Coupled
+
+
+namespace Composition2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}

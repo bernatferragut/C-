10e30856a389cd4db0cf1358fbf11ddc12965f7a
Bernat Ferragut <elbernat@gmail.com>
2017-02-09 10:13:36 -0300

    AM

--- 2_Intermediate/10_AccessModifiers/AccessModifiers/Amazon/Customer.cs
+++ 2_Intermediate/10_AccessModifiers/AccessModifiers/Amazon/Customer.cs
@@ -2,38 +2,18 @@ using System;
 
 namespace Amazon
 {
-
 	public class Customer
 	{
-		public int Id
-		{
-			get;
-			set;
-		}
+		public int Id { get; set; }
 
-		public string Name
-		{
-			get;
-			set;
-		}
+		public string Name { get; set; }
 
 		public void Promote() //  should be encapsulated
 		{
-			var rating = CalculateRating();
-			if (rating == 0)
-			{
-				Console.WriteLine("Promoted to Level 1");
-			}
-			else
-			{
-				Console.WriteLine("Promoted to Level 2");
-			}
-		}
-		// avoid Protected
+			Console.WriteLine("Promote Logic changed");
 
-		private int CalculateRating() // this method should not be available outside > private // not use protected
-		{
-			return 0;
+			var calculator = new Ratecalculator();
+			var rating = calculator.Calculate(this);
 		}
 	}
 }

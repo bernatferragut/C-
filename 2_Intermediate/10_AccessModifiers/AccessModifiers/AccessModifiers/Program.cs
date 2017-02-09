a58a8335c7c981cd2b011533bd20ee9230997548
Bernat Ferragut <elbernat@gmail.com>
2017-02-09 09:43:06 -0300

    AM

--- 2_Intermediate/10_AccessModifiers/AccessModifiers/AccessModifiers/Program.cs
+++ 2_Intermediate/10_AccessModifiers/AccessModifiers/AccessModifiers/Program.cs
@@ -6,7 +6,28 @@ namespace AccessModifiers
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var customer = new Customer();
+			customer.Promote();
+		}
+	}
+
+	public class Customer
+	{
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public void Promote()
+		{
+			
 		}
 	}
 }

b512717d9e581a637b8cd91b468d3a8eeea37baa
Bernat Ferragut <elbernat@gmail.com>
2017-02-09 09:47:15 -0300

    AM

--- 2_Intermediate/10_AccessModifiers/AccessModifiers/AccessModifiers/Program.cs
+++ 2_Intermediate/10_AccessModifiers/AccessModifiers/AccessModifiers/Program.cs
@@ -25,9 +25,22 @@ namespace AccessModifiers
 			set;
 		}
 
-		public void Promote()
+		public void Promote() //  should be encapsulated
 		{
-			
+			var rating = CalculateRating();
+			if (rating == 0)
+			{
+				Console.WriteLine("Promoted to Level 1");
+			}
+			else
+			{
+				Console.WriteLine("Promoted to Level 2");
+			}
+		}
+
+		public int CalculateRating() // this method should not be available outside
+		{
+			return 0;
 		}
 	}
 }

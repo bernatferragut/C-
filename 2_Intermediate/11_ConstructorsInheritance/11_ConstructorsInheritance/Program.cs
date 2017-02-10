70db0f504d04ef86abc610f7a97bb860d5a057a1
Bernat Ferragut <elbernat@gmail.com>
2017-02-10 11:08:12 -0300

    Ctors and Inheritance

--- 2_Intermediate/11_ConstructorsInheritance/11_ConstructorsInheritance/Program.cs
+++ 2_Intermediate/11_ConstructorsInheritance/11_ConstructorsInheritance/Program.cs
@@ -2,11 +2,32 @@
 
 namespace _ConstructorsInheritance
 {
+
+	// During Object Istantiation:
+	// 1. Base class constructors always executed first
+	// 2. Base class construcors are not inherited
+
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+		}
+	}
+
+	public class Vehicle
+	{
+		public Vehicle()
+		{
+			Console.WriteLine("Vehicle is being initialized");
+		}
+	}
+
+	public class Car : Vehicle
+	{
+		public Car()
+		{
+			Console.WriteLine("Car is being initialized");
 		}
 	}
 }

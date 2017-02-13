359396933378cbeb593fccfc62166187da1dc690
Bernat Ferragut <elbernat@gmail.com>
2017-02-13 08:45:45 -0300

    Method Overriding

--- 2_Intermediate/12_MethodOverriding/12_MethodOverriding/Program.cs
+++ 2_Intermediate/12_MethodOverriding/12_MethodOverriding/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Method Overriding
+// Modifyinf the implementation of an inherited Method
+// We need to encapsulate data and behaviour in one Class
+
+using System;
 
 namespace _MethodOverriding
 {
@@ -7,6 +11,36 @@ namespace _MethodOverriding
 		public static void Main(string[] args)
 		{
 			// Code
+		}
+	}
+
+	public class Shape
+	{
+		public int Width { get;set; }
+		public int Height { get;set; }
+		public Position Position { get; set; }
+
+		public virtual void Draw()
+		{
+			
+		}
+	}
+
+	public class Circle : Shape
+	{
+		
+	}
+
+	public class Rectangle : Shape
+	{
+		
+	}
+
+	public class Canvas
+	{
+		public void DrawShapes(List<Shape> shapes)
+		{
+			
 		}
 	}
 }

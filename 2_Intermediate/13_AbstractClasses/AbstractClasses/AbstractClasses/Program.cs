de57bbc171a555a3f7b46fbe58b3e02b9b3c1975
Bernat Ferragut <elbernat@gmail.com>
2017-02-14 09:25:00 -0300

    Abstract Classes

--- 2_Intermediate/13_AbstractClasses/AbstractClasses/AbstractClasses/Program.cs
+++ 2_Intermediate/13_AbstractClasses/AbstractClasses/AbstractClasses/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Abstract Classes
+// Indicates that a class or member is missing implementation
+
+using System;
 
 namespace AbstractClasses
 {
@@ -7,6 +10,24 @@ namespace AbstractClasses
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+		}
+	}
+
+	// OLD EXAMPLE
+
+	public class Shape
+	{
+		public virtual void Draw() // virtual
+		{
+			// How to draw a shape ?
+		}
+	}
+
+	public class Circle : Shape
+	{
+		public override void Draw()
+		{
+			Console.WriteLine(" Draw a circle");
 		}
 	}
 }

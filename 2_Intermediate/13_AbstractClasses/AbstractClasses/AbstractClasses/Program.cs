ba2fd918b72e0923ad6a285a23e08ddc886145a3
Bernat Ferragut <elbernat@gmail.com>
2017-02-14 09:42:19 -0300

    abstract modifier

--- 2_Intermediate/13_AbstractClasses/AbstractClasses/AbstractClasses/Program.cs
+++ 2_Intermediate/13_AbstractClasses/AbstractClasses/AbstractClasses/Program.cs
@@ -9,25 +9,51 @@ namespace AbstractClasses
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var circle = new Circle();
+			circle.Draw();
+
+			var rectangle = new Rectangle();
+			rectangle.Draw();
+				
 		}
 	}
 
-	// OLD EXAMPLE
-
-	public class Shape
+	public class Rectangle : Shape
 	{
-		public virtual void Draw() // virtual
+		public void Draw()
 		{
-			// How to draw a shape ?
+			Console.WriteLine("Draw a Rectangle");
 		}
 	}
 
-	public class Circle : Shape
-	{
-		public override void Draw()
-		{
-			Console.WriteLine(" Draw a circle");
-		}
-	}
+	//// OLD EXAMPLE
+	//public class Shape
+	//{
+	//	public virtual void Draw() // virtual
+	//	{
+	//		// How to draw a shape ? The concept of a shape is too abstact
+	//	}
+	//}
+
+	//public class Circle : Shape
+	//{
+	//	public override void Draw()
+	//	{
+	//		Console.WriteLine(" Draw a circle");
+	//	}
+	//}
+
+	//// WITH THE ABSTRACT MODIFIER
+	//public abstract class Shape2 // abstarct class // They cannot be instantiated
+	//{
+	//	public abstract void Draw2(); // abstract method pass without implemention
+	//}
+
+	//public class Circle2 : Shape2
+	//{
+	//	public override void Draw2()
+	//	{
+	//		// Downstream the method is implemented
+	//	}
+	//}
 }
---
+++ 2_Intermediate/13_AbstractClasses/AbstractClasses/AbstractClasses/Shape.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AbstractClasses
+{
+	public class Shape
+	{
+		public int Width { get;set; }
+		public int Height { get; set; }
+
+		public virtual void Draw()
+		{
+			
+		}
+	}
+}

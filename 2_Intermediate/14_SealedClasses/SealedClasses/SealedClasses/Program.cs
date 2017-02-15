92df81b64338770a79b4a18136f436f80002bdfd
Bernat Ferragut <elbernat@gmail.com>
2017-02-15 07:42:40 -0300

    Sealed Class

--- 2_Intermediate/14_SealedClasses/SealedClasses/SealedClasses/Program.cs
+++ 2_Intermediate/14_SealedClasses/SealedClasses/SealedClasses/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Sealed Classes and Members
+// Prevents derivation of classes or overriding of Methods
+
 
 using System;
 
@@ -9,6 +11,16 @@ namespace SealedClasses
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+		}
+	}
+
+	// Overriding Class Example
+	// We cannot make a class derived from Circle
+	public sealed class Circle : Shape
+	{
+		public override void Draw();
+		{
+			Console.WriteLine ("Drawing a Circle");
 		}
 	}
 }

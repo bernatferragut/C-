9552f44e8818525ea2fd83eecf5ce3b3390ec824
Bernat Ferragut <elbernat@gmail.com>
2017-02-15 07:37:57 -0300

    Sealed Classes

---
+++ 2_Intermediate/14_SealedClasses/SealedClasses/SealedClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿// Sealed Classes and Members
+
+using System;
+
+namespace SealedClasses
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}

f6a856902ac804d57799cf6e0bd1f2857d64ceb0
Bernat Ferragut <elbernat@gmail.com>
2017-02-15 07:44:53 -0300

    Sealed Classes

--- 2_Intermediate/14_SealedClasses/SealedClasses/SealedClasses/Program.cs
+++ 2_Intermediate/14_SealedClasses/SealedClasses/SealedClasses/Program.cs
@@ -16,6 +16,9 @@ namespace SealedClasses
 
 	// Overriding Class Example
 	// We cannot make a class derived from Circle
+	// Sealed Classes are slightly faster because of run-time optimizations
+	// Hardly ever used
+	// As a guideline do not use it
 	public sealed class Circle : Shape
 	{
 		public override void Draw();

068c55cc3b1dee2208e8bf028e129bf2ea888a72
Bernat Ferragut <elbernat@gmail.com>
2017-02-17 08:00:28 -0300

    Poly Exercices

--- 2_Intermediate/15_Polymorphism/Exercices/Exercices/Program.cs
+++ 2_Intermediate/15_Polymorphism/Exercices/Exercices/Program.cs
@@ -15,6 +15,35 @@ namespace Exercices
 			myOracleConnection.OpenConnection();
 		}
 	}
+
+	// Class DbCommand that executes database instructions
+
+	public class DbCommand
+	{
+		private DbConnection _dbConnection; // prop
+
+		public DbConnection dbConnection // prop logic
+		{
+			get
+			{
+				return _dbConnection;
+			}
+			set
+			{
+				if (dbConnection == null)
+				{
+					throw new NullReferenceException("The string is Null");
+				}
+				_dbConnection = value;
+			}
+		}
+
+		public DbCommand(DbConnection dbConnection)
+		{
+			_dbConnection = dbConnection;
+		}
+	}
+
 }
 
 

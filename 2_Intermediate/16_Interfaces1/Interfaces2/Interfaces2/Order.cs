d385224afb86a5794fb196950887785ddcaf8763
Bernat Ferragut <elbernat@gmail.com>
2017-02-18 11:43:11 -0300

    Interfaces

--- 2_Intermediate/16_Interfaces1/Interfaces2/Interfaces2/Order.cs
+++ 2_Intermediate/16_Interfaces1/Interfaces2/Interfaces2/Order.cs
@@ -2,9 +2,16 @@ using System;
 
 namespace Interfaces2
 {
-
 	public class Order
 	{
-		
+		public int Id { get;set; }
+		public DateTime DateTimePlaced { get; set; }
+		public Shippment Shippment { get; set; }
+		public float TotalPrice { get; set; }
+
+		public bool IsShipped
+		{
+			get { return Shippment != null; }
+		}
 	}
 }

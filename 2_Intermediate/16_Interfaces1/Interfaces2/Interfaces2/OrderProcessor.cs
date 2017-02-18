045c2b9717706ac1ae44d0a6aea219efd81b5d36
Bernat Ferragut <elbernat@gmail.com>
2017-02-18 11:19:24 -0300

    Interfaces

--- 2_Intermediate/16_Interfaces1/Interfaces2/Interfaces2/OrderProcessor.cs
+++ 2_Intermediate/16_Interfaces1/Interfaces2/Interfaces2/OrderProcessor.cs
@@ -2,9 +2,25 @@ using System;
 
 namespace Interfaces2
 {
-
 	public class OrderProcessor
 	{
-		
+		private readonly ShippingCalculator _shippingCalculator;
+
+		public OrderProcessor()
+		{
+			_shippingCalculator = new ShippingCalculator();
+		}
+
+		public void Processor(Order order)
+		{
+			if (order.IsShipped) // def programming
+				throw new InvalidOperationException("The order is already processed");
+			
+			order.Shipment = new Shipment; // object creation 
+			{
+				Cost = _shippingCalculator.CalculatorShipping(order),
+				ShippingDate = DateTime.Today.AddDays(1)
+			};
+		}
 	}
 }
--- 2_Intermediate/16_Interfaces1/Interfaces2/Interfaces2/ShippingCalculator.cs
+++ 2_Intermediate/16_Interfaces1/Interfaces2/Interfaces2/ShippingCalculator.cs
@@ -2,9 +2,19 @@ using System;
 
 namespace Interfaces2
 {
+	public interface IShippingCalculator // Interface
+	{
+		
+	}
 
 	public class ShippingCalculator
 	{
-		
+		public float CalculateShipping(Order order)
+		{
+			if (order.TotalPrice < 30)
+				return order.TotalPrice * .1f;
+
+			return 0;
+		}
 	}
 }

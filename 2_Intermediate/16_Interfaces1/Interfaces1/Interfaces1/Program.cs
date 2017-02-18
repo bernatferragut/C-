9c216a7575066f9e41bc19465309c5967b0244c8
Bernat Ferragut <elbernat@gmail.com>
2017-02-18 10:47:35 -0300

    Interfaces

--- 2_Intermediate/16_Interfaces1/Interfaces1/Interfaces1/Program.cs
+++ 2_Intermediate/16_Interfaces1/Interfaces1/Interfaces1/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Interface
+// Similar to a class from in syntaz terms but fundamentally different
+// In .Net all interfaces start with the letter I'
+// You call Methods but do not Implement them like in a class
+// Do not have acces modifiers
+// They meant to build loosely copled apps
+
+using System;
 
 namespace Interfaces1
 {

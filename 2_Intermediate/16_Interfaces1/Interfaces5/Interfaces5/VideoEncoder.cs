625708d02b87d758eb48ee5e7901a5ac19b196bf
Bernat Ferragut <elbernat@gmail.com>
2017-02-22 22:21:19 -0300

    Open Close Principle Implementation

--- 2_Intermediate/16_Interfaces1/Interfaces5/Interfaces5/VideoEncoder.cs
+++ 2_Intermediate/16_Interfaces1/Interfaces5/Interfaces5/VideoEncoder.cs
@@ -1,22 +1,26 @@
 // Interfaces and Polymorphism
 
 using System;
+using System.Collections.Generic;
 
 namespace Interfaces5
 {
 	public class VideoEncoder
 	{
-		private readonly MailService _mailService; // prop
+		private List<INotificationChannel> _notificationChannels;
 
 		public VideoEncoder() // ctor
 		{
-			_mailService = new MailService();
+			_notificationChannels = new List<INotificationChannel>();
 		}
 
 		public void Encode(Video video)
 		{
 			// Video Logic
-			_mailService.Send(new Mail());
+
+			foreach (var channel in _notificationChannels)
+				channel.Send(new Message());
+
 		}
 
 	}
